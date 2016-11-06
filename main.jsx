20d04e75457bc6d2b94846a53fae2e0d4c1056ec
meow <meow@iceland>
2016-11-06 12:57:42 -0800

    Canonical Version

--- main.jsx
+++ main.jsx
@@ -48,18 +48,13 @@ const TopMenu = function({ active }) {
 
 const HeroImage = function() {
    return (
-<<<<<<< HEAD
       <section id="HeroContainer">
          <div id="-container">
             <button href="#">Request a Appointment</button>
-=======
-      <section id="HeroImage">
-         <div>
-            <img src="heroimage.jpg"/>
+            <img src=""/>
             <div id="hilink-container">
                 <a href="#">Request a Appointment</a>
             </div>
->>>>>>> c2c5c3fa93b3a14e742eaeb8be84d8d317f072db
          </div>
       </section>
    )
@@ -93,185 +88,6 @@ const Footer = function() {
    )
 }
 
-<<<<<<< HEAD
-const TaxRateMenu = function ({ active }) {
-    let a, b, c, d, e;
-    a = b = c = d = e = "item inactive"
-    switch (active) {
-        case "Single":
-            a = "item active"
-            break
-        case "Maried Filing Jointly & Surviving Spouses":
-            b = "item active"
-            break
-        case "Married Filing Separately":
-            c = "item active"
-            break
-        case "Head of Household":
-            d = "item active"
-            break
-        case "Estate & Trusts":
-            e = "item active"
-            break
-    }
-
-    return (
-      <section id="TaxRateMenu">
-        <h4>Tax Rates</h4>
-        <table>
-            <thead>
-                <nav className="taxratenav">
-                    <div className="">
-                        <a className={a} href="#">Single</a>
-                        <a className={b} href="#">Married Filing Jointly
-                        & Surviving Spouses</a>
-                        <a className={c} href="#">Married Filing Separately</a>
-                        <a className={d} href="#">Head of Household</a>
-                        <a className={e} href="#">Estate & Trusts</a>
-                    </div>
-                </nav>
-            </thead>
-            <tbody>
-                <tr>
-                   <td>10%</td>
-                   <td>0 to $9,275</td>
-                </tr>
-                <tr>
-                   <td>15%</td>
-                   <td>$9,275 to $37,650</td>
-                </tr>
-                <tr>
-                   <td>25%</td>
-                   <td>$37,650 to $91,150</td>
-                </tr>
-                <tr>
-                   <td>28%</td>
-                   <td>$91,150 to $190,150</td>
-                </tr>
-                <tr>
-                   <td>33%</td>
-                   <td>$190,150 to $413,350</td>
-                </tr>
-                <tr>
-                   <td>35%</td>
-                   <td>$413,350 to $415,050</td>
-                </tr>
-                <tr>
-                   <td>39.6%</td>
-                   <td>Over $415,050</td>
-                </tr>
-                <tr>
-                    <td>10%</td>
-                    <td>0 to $18,550</td>
-                </tr>
-                <tr>
-                   <td>15%</td>
-                   <td>$18,550 to $75,300</td>
-                </tr>
-                <tr>
-                   <td>25%</td>
-                   <td>$75,300 to $151,900</td>
-                </tr>
-                <tr>
-                   <td>28%</td>
-                   <td>$151,900 to $231,450</td>
-                </tr>
-                <tr>
-                   <td>33%</td>
-                   <td>$231,450 to $413,350</td>
-                </tr>
-                <tr>
-                   <td>35%</td>
-                   <td>$413,350 to $466,950</td>
-                </tr>
-                <tr>
-                   <td>39.6%</td>
-                   <td>Over $466,950</td>
-                </tr>
-                <tr>
-                   <td>10%</td>
-                   <td>0 to $9,275</td>
-                </tr>
-                <tr>
-                   <td>15%</td>
-                   <td>$9,275 to $37,650</td>
-                </tr>
-                <tr>
-                   <td>25%</td>
-                   <td>$75,950 to $115,725</td>
-                </tr>
-                <tr>
-                   <td>28%</td>
-                   <td>$115,725 to $206,675</td>
-                </tr>
-                <tr>
-                   <td>33%</td>
-                   <td>$206,675 to $233,475</td>
-                </tr>
-                <tr>
-                   <td>35%</td>
-                   <td>$206,675 to $233,475</td>
-                </tr>
-                <tr>
-                   <td>39.6%</td>
-                   <td>Over $233,475</td>
-                </tr>
-                <tr>
-                   <td>10%</td>
-                   <td>0 to $13,250</td>
-                </tr>
-                <tr>
-                   <td>15%</td>
-                   <td>$13,250 to $50,400</td>
-                </tr>
-                <tr>
-                   <td>25%</td>
-                   <td>$50,400 to $130,150</td>
-                </tr>
-                <tr>
-                   <td>28%</td>
-                   <td>$130,150 to $210,800</td>
-                </tr>
-                <tr>
-                   <td>33%</td>
-                   <td>$210,800 to $413,350</td>
-                </tr>
-                <tr>
-                   <td>35%</td>
-                   <td>$413,350 to $441,000</td>
-                </tr>
-                <tr>
-                   <td>39.6%</td>
-                   <td>Over $441,000</td>
-                </tr>
-                <tr>
-                   <td>10%</td>
-                   <td>0 to $2,550</td>
-                </tr>
-                <tr>
-                   <td>25%</td>
-                   <td>$2,550 to $5,950</td>
-                </tr>
-                <tr>
-                   <td>28%</td>
-                   <td>$5,950 to $9,050</td>
-                </tr>
-                <tr>
-                   <td>33%</td>
-                   <td>$9,050 to $12,400</td>
-                </tr>
-                <tr>
-                   <td>39.6%</td>
-                   <td>Over $12,400</td>
-                </tr>
-            </tbody>
-        </table>
-      </section>
-    )
-}
-
-=======
->>>>>>> c2c5c3fa93b3a14e742eaeb8be84d8d317f072db
 //------------------------------------------------------------------------------
 // View
 
